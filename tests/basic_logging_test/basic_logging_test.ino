bc7209a9dbd8d561d96f857ccc0c38c762508aa5
Dan Egnor <egnor@ofb.net>
2025-10-30 20:41:26 -0700

    use scenario testing again

--- tests/basic_logging_test/basic_logging_test.ino
+++ tests/basic_logging_test/basic_logging_test.ino
@@ -4,15 +4,19 @@ OkLoggingContext OK_CONTEXT("log_tag");
 
 void setup() {
   Serial.begin(115200);
-  OK_DETAIL("Detail");
-  OK_NOTE("Note");
+  OK_DETAIL("Detail message");
+  OK_DETAIL("");
+  OK_NOTE("Note message");
   OK_NOTE("\n\nNote with newlines before and after\n\n");
   OK_NOTE("Note with\nnewlines in the\nmiddle");
-  OK_ERROR("Error");
+  OK_NOTE("");
+  OK_ERROR("Error message");
+  OK_ERROR("");
   OK_ERROR("\n\nError with newlines before and after\n\n");
+  OK_FATAL("Fatal message");
 }
 
 void loop() {
-  Serial.println("+++SIMDONE");
+  Serial.println("FAIL");  // should not be reached
   delay(1000);
 }
--- tests/run_tests.py
+++ tests/run_tests.py
@@ -5,10 +5,12 @@ import json
 import logging
 import ok_logging_setup
 import ok_subprocess_defaults
+import subprocess
 import toml
 from pathlib import Path
 
 ok_logging_setup.install()
+ok_logging_setup.skip_traceback_for(subprocess.CalledProcessError)
 sub = ok_subprocess_defaults.SubprocessDefaults()
 
 parser = argparse.ArgumentParser()
@@ -16,31 +18,10 @@ parser.add_argument("test_dir", nargs="*", type=Path)
 args = parser.parse_args()
 
 tests_dir = Path(__file__).parent
-for test_dir in args.test_dir or tests_dir.glob("*_test"):
+test_dirs = list(args.test_dir or tests_dir.glob("*_test"))
+for test_dir in test_dirs:
     logging.info(f"\n🧪 TEST: {test_dir}/")
-    output_dir = tests_dir / "build.tmp" / test_dir.name
-    sub.run("arduino-cli", "compile", test_dir, f"--output-dir={output_dir}")
+    sub.run("arduino-cli", "compile", test_dir, f"--export-binaries")
+    sub.run("wokwi-cli", test_dir, "--scenario=scenario.yaml")
 
-    wokwi_config = {
-      "version": 1,
-      "firmware": test_dir.name + ".ino.bin",
-      "elf": test_dir.name + ".ino.elf",
-    }
-    (output_dir / "wokwi.toml").write_text(toml.dumps({"wokwi": wokwi_config}))
-
-    wokwi_diagram = {
-      "parts": [{"type": "board-esp32-devkit-c-v4", "id": "esp", "attrs": {}}],
-      "connections": [
-        ["esp:TX", "$serialMonitor:RX", ""],
-        ["esp:RX", "$serialMonitor:TX", ""],
-      ],
-    }
-    (output_dir / "diagram.json").write_text(json.dumps(wokwi_diagram))
-
-    serial_log_path = output_dir / "serial_log.txt"
-    sub.run(
-        "wokwi-cli", output_dir,
-        "--expect-text=+++SIMDONE",
-        "--fail-text=+++SIMFAIL",
-        f"--serial-log-file={serial_log_path}",
-    )
+logging.info(f"\n🎉 All {len(test_dirs)} tests passed")
