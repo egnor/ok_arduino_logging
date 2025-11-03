3da6fa54b3e665b5dcc0e91293e1fd73f1b46edd
Dan Egnor <egnor@ofb.net>
2025-11-03 07:42:25 -0800

    fix tests

--- tests/basic_logging/basic_logging_test.py
+++ tests/basic_logging/basic_logging_test.py
@@ -1,2 +1,26 @@
+import re
+import re_assert
+import textwrap
+
 def test_basic_logging(sim_output_dir):
-    pass  # scenario.yaml makes the important assertions
+    matcher = re_assert.Matches(textwrap.dedent(r"""
+        .*
+        [\d.]+ \[log_tag\] Note message
+
+        [\d.]+ \[log_tag\] Note with newlines before and after
+        [\d.]+ \[log_tag\] Note with
+        newlines in the
+        middle
+
+        [\d.]+ ⚠️ \[log_tag\] Error message
+        [\d.]+ ⚠️ \[log_tag\] ERROR
+
+        [\d.]+ ⚠️ \[log_tag\] Error with newlines before and after
+        [\d.]+ 💥 \[log_tag\] FATAL Fatal message
+          at: .*/basic_logging.ino:\d+
+          in: void setup\(\)
+          🚨 REBOOT IN 1 SEC 🚨
+    """).lstrip(), re.DOTALL | re.MULTILINE)
+
+    log = (sim_output_dir / "serial_log.txt").read_text().replace("\r", "")
+    matcher.assert_matches(log)
